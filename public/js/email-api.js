a3347f6bf62020bf35861ea048a3e39927af1951
federicolaterra <f.laterra@pharmatrace.io>
2022-08-26 15:19:24 +0200

    added new API (test) for Requirement gathering (PT)

--- public/js/email-api.js
+++ public/js/email-api.js
@@ -12,10 +12,14 @@ const emailApi = {
                 sound.play();
             }, 500);
             setTimeout(() => {
+                // select which method
                 this.sendTextPlainEmail();
             }, 2000);
         });
     },
+    /**
+     * Send POST API with only Text Plain (EV)
+     */
     sendTextPlainEmail() {
         const url = '/api/test-email';
         axios.post(url)
@@ -34,6 +38,50 @@ const emailApi = {
                 console.log(error);
         })
     },
+    /**
+     * Send POST API with only Text Plain for (PT)
+     */
+    sendTextPlainEmailPharmatrace() {
+        const url = '/api/pharmatrace/test-email';
+        const options = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        };
+        const to = 'f.laterra@pharmatrace.io';
+        const dynamic_template_data = {
+            subject: 'Requirement gathering form: CMO/CPO [Company Name]',
+            main_text: "We have successfully received data from the customer",
+            sub_text: 'Please find below the submitted information:',
+            form_fields_content: [
+                { key:  'field1', value: 'value1' },
+                { key:  'field2', value: 'value2' },
+                { key:  'field3', value: 'value3' }
+            ]
+        }
+        axios.post(url, {
+            to: to,
+            dynamic_template_data: dynamic_template_data
+        }, options)
+            .then((res) => {
+                if (res.data.statusCode === 202) {
+                    document.querySelector('section').style.backgroundColor = 'rgba(68, 207, 136, 0.2)';
+                    document.querySelector('section .content').innerHTML = `
+                        <i data-feather="check-circle" style="stroke: #44cf87"></i>
+                        <h1 style="color: #44cf87">The email was sent successfully!</h1>
+                    `;
+                    feather.replace();
+                    document.querySelector('.content').classList.toggle('hide');
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+        })
+    },
+    /**
+     * Send POST API with Dynamic Template (EV)
+     */
     sendEmailWithTemplate() {
         const url = '/api/test-email-template';
         const options = {
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -35,5 +35,33 @@ router.post('/api/test-email-template', async (req, res) => {
     }
 });
 
+router.post('/api/pharmatrace/test-email', async (req, res) => {
+    /**
+     * I build the dynamic object of the fields
+     */
+    const formFieldsContent = req.body.dynamic_template_data.form_fields_content.map(obj => {
+        return `
+            <li><strong>${obj.key}:</strong> ${obj.value}</li>
+        `
+    })
+
+    try {
+        const responseEmail = await sendEmail.sendTextPlainEmail({
+            to: req.body.to,
+            from: 'f.laterra@pharmatrace.io',
+            subject: req.body.dynamic_template_data.subject,
+            text: req.body.dynamic_template_data.main_text,
+            html:`
+                <h3>${req.body.dynamic_template_data.main_text}</h3>
+                <p><strong>${req.body.dynamic_template_data.sub_text}</strong></p>
+                <ul>${formFieldsContent}</ul>
+            `
+        });
+        res.send(responseEmail[0]);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
 module.exports = router;
 
